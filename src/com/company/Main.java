99bf4469c87fc942a71d9dc48d4191f7d492ffce
barhom <barhom@knights.ucf.edu>
2021-09-13 23:10:39 -0400

    aalko-cop3330-ex07

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+
+
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the length of the room in feet?");
+        int length=sc.nextInt();
+        System.out.println("What is the width of the room in feet?");
+        int width=sc.nextInt();
+        System.out.println("You entered dimensions of "+length+" by "+width+" feet.");
+
+
+        int area=length*width;
+        final double conversion = area*0.093;
+
+
+        System.out.println("The area is");
+        System.out.println(area+" square feet");
+        System.out.println(conversion+" square meters");
+    }
+}
